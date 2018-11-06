a4c52c78d2627ee7fd9933a01851e19fd79b879e
Tomáš Javorský <tomuso.o@gmail.com>
2018-11-06 14:36:29 +0100

    Download from remote, cache to disk and load from disk async working. Sprite is loaded on image upon completion.

---
+++ Assets/ImageLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class ImageLoader : MonoBehaviour {
+
+	public AssetReferenceSprite imageToLoad;
+
+	void Start () {
+		imageToLoad.LoadAsset().Completed += (op) =>
+		{
+			GetComponent<Image>().sprite = op.Result;
+		};
+	}
+}
